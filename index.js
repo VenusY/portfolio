488c858701d960c9e9a66f616d1124581d37c7c2
Venus <venusyip@outlook.com>
2024-10-18 11:24:36 +0100

    Add AJAX form submission

--- index.js
+++ index.js
@@ -5,68 +5,84 @@ function validateInput(inputField, ...errorMessages) {
       inputField.parentElement.classList.add('contact-form__wrapper--error');
       errorMessages[1].classList.add('contact-form__error-message--visible');
       errorMessages[0].classList.remove('contact-form__error-message--visible');
-      return;
+      return false;
     }
   }
 
-  if (!inputField.value) {
-    inputField.parentElement.classList.add('contact-form__wrapper--error');
-    errorMessages[0].classList.add('contact-form__error-message--visible');
-  } else {
-    inputField.parentElement.classList.remove('contact-form__wrapper--error');
-    errorMessages[0].classList.remove('contact-form__error-message--visible');
-  }
-
+  /**
+   * If the function gets to this point, then the invalid error message on the
+   * email input field is no longer needed, so it is removed.
+   */
   try {
     errorMessages[1].classList.remove('contact-form__error-message--visible');
   } catch (error) {
     // Do nothing
   }
+
+  if (!inputField.value) {
+    inputField.parentElement.classList.add('contact-form__wrapper--error');
+    errorMessages[0].classList.add('contact-form__error-message--visible');
+    return false;
+  } else {
+    inputField.parentElement.classList.remove('contact-form__wrapper--error');
+    errorMessages[0].classList.remove('contact-form__error-message--visible');
+    return true;
+  }
 }
 
-async function handleSubmit(e) {
+function handleSubmit(e) {
+  const nameInputField = document.querySelector('.contact-form__name');
+  const emailAddressInputField = document.querySelector(
+    '.contact-form__email-address',
+  );
+  const messageInputField = document.querySelector('.contact-form__message');
+  const nameErrorMessage = document.querySelector(
+    '.contact-form__error-message--name',
+  );
+  const emailEmptyErrorMessage = document.querySelector(
+    '.contact-form__error-message--email-empty',
+  );
+  const emailInvalidErrorMessage = document.querySelector(
+    '.contact-form__error-message--email-invalid',
+  );
+  const messageErrorMessage = document.querySelector(
+    '.contact-form__error-message--message',
+  );
+
+  const results = [
+    validateInput(nameInputField, nameErrorMessage),
+    validateInput(
+      emailAddressInputField,
+      emailEmptyErrorMessage,
+      emailInvalidErrorMessage,
+    ),
+    validateInput(messageInputField, messageErrorMessage),
+  ];
+
+  if (results.includes(false)) {
+    return;
+  }
+
   const contactForm = document.querySelector('.contact-form');
   const formData = new FormData(contactForm);
   const data = Object.fromEntries(formData);
 
-  const nameInputField = document.querySelector('.contact-form__name');
-  const emailAddressInputField = document.querySelector(
-    '.contact-form__email-address'
-  );
-  const messageInputField = document.querySelector('.contact-form__message');
-  const nameErrorMessage = document.querySelector(
-    '.contact-form__error-message--name'
-  );
-  const emailEmptyErrorMessage = document.querySelector(
-    '.contact-form__error-message--email-empty'
-  );
-  const emailInvalidErrorMessage = document.querySelector(
-    '.contact-form__error-message--email-invalid'
-  );
-  const messageErrorMessage = document.querySelector(
-    '.contact-form__error-message--message'
-  );
+  try {
+    fetch('https://formsubmit.co/ajax/venusyip@outlook.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-  validateInput(nameInputField, nameErrorMessage);
-  validateInput(
-    emailAddressInputField,
-    emailEmptyErrorMessage,
-    emailInvalidErrorMessage
-  );
-  validateInput(messageInputField, messageErrorMessage);
-
-  // try {
-  //   const response = await fetch('https://formsubmit.co/venusyip@outlook.com', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Accept: 'application/json',
-  //     },
-  //     body: JSON.stringify(data),
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  nameInputField.value = '';
+  emailAddressInputField.value = '';
+  messageInputField.value = '';
 }
 
 const contactForm = document.querySelector('.contact-form');
@@ -81,7 +97,7 @@ function setOffset() {
 
   navBarHeight = navBar.offsetHeight;
   sectionHeadings.forEach(
-    heading => (heading.style.scrollMarginTop = `${navBarHeight}px`)
+    heading => (heading.style.scrollMarginTop = `${navBarHeight}px`),
   );
 }
 
